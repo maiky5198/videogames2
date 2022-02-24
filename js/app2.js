8d20725fcd3ef82a1dc7c3439b45a07e2593bd24
maiky5198 <maiky5198@gmail.com>
2022-02-24 09:49:59 -0500

    update

--- js/app2.js
+++ js/app2.js
@@ -28,8 +28,10 @@ canvas.addEventListener('mouseup', function(){
 //player
 class Player {
       constructor(){
-          this.x = canvas.width;
-          this.y = canvas.height/2;
+          //this.x = canvas.width;
+          this.x = 0;
+          this.y = 0;
+          //this.y = canvas.height/2;
           this.radius = 50;
           this.angle = 0;
           this.frameX = 0;
@@ -42,10 +44,10 @@ class Player {
          const dx = this.x - mouse.x;
          const dy = this.y - mouse.y;
          if (mouse.x != this.x) {
-             this.x -= dx/30;
+             this.x -= dx/10;
          }
          if(mouse.y != this.y) {
-             this.y -= dy/30;
+             this.y -= dy/10;
          }
         }
         draw(){
@@ -56,7 +58,7 @@ class Player {
             ctx.lineTo(mouse.x, mouse.y);
             ctx.stroke();
                 }
-          ctx.fillStyle = 'red';
+          ctx.fillStyle = 'brown';
           ctx.beginPath();
           ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);  
           ctx.fill();
@@ -64,7 +66,29 @@ class Player {
         }
       }
 const player = new Player();
-//bubbles
+//cheese
+const cheeseArray = [];
+class Cheese {
+    constructor(){
+         this.x = Math.random() * canvas.width;
+         this.y = Math.random() * canvas.heights;
+         this.radius = 50;
+         this.speed = Math.random() * 5 + 1;
+         this.distance;
+          }
+    update(){
+        this.y -= this.speed;
+    }
+    draw(){
+        ctx.fillStyle = 'blue';
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        ctx.fill();
+        ctx.closePath();
+    }      
+}
+
+
 //animation loop
 function animate(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
